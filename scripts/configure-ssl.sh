bf399d5998b7940d075b6b31dd2b88efd560943e
Dmitry Shmulevich <dshmulevich@nvidia.com>
2025-03-14 08:37:24 -0700

    extend validity of generated certificates
    Signed-off-by: Dmitry Shmulevich <dshmulevich@nvidia.com>

--- scripts/configure-ssl.sh
+++ scripts/configure-ssl.sh
@@ -13,8 +13,8 @@ SRV_CERT="${SSL_DIR}/server-cert.pem"
 
 mkdir -p "${SSL_DIR}"
 openssl genrsa -out ${CA_KEY} 4096
-openssl req -x509 -new -nodes -key ${CA_KEY} -sha512 -out ${CA_CERT} -subj "/CN=*"
+openssl req -x509 -new -nodes -key ${CA_KEY} -sha512 -out ${CA_CERT} -subj "/CN=*" -days 365
 openssl genrsa -out ${SRV_KEY} 4096
-openssl req -new -key ${SRV_KEY} -out ${SRV_CSR} -subj "/O=nvidia/CN=localhost"
+openssl req -new -key ${SRV_KEY} -out ${SRV_CSR} -subj "/O=nvidia/CN=localhost" -days 365
 openssl x509 -req -in ${SRV_CSR} -CA ${CA_CERT} -CAkey ${CA_KEY} -CAcreateserial -out ${SRV_CERT} -sha512
 openssl verify -CAfile ${CA_CERT} ${SRV_CERT}
