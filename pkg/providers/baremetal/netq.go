847a57f025f41b21651791e1f6b2200770e1d2d6
Ritika Srivastava <risrivastava@nvidia.com>
2025-06-05 19:10:43 -0700

    Parse Netq output (#94)
    * Support for reading topology from Netq
    * Merge the Clos network
    
    Signed-off-by: Ritika Srivastava <risrivastava@nvidia.com>

---
+++ pkg/providers/baremetal/netq.go
@@ -0,0 +1,227 @@
+/*
+ * Copyright (c) 2025, NVIDIA CORPORATION.  All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package baremetal
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"sort"
+	"strings"
+
+	"github.com/NVIDIA/topograph/internal/exec"
+	"github.com/NVIDIA/topograph/pkg/topology"
+)
+
+type NetqResponse struct {
+	Links []Links `json:"links"`
+	Nodes []Nodes `json:"nodes"`
+}
+
+type Nodes struct {
+	Cnode []CNode `json:"compounded_nodes"`
+}
+
+type CNode struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+	Tier int    `json:"tier"`
+}
+
+type Links struct {
+	CompoundedLinks []CompoundedLinks `json:"compounded_links"`
+	Id              string            `json:"id"`
+}
+
+type CompoundedLinks struct {
+	Id string `json:"id"`
+}
+
+type AuthOutput struct {
+	AccessToken string `json:"access_token"`
+}
+
+func generateTopologyConfigForEth(ctx context.Context, cred Credentials, p ProviderParams, cis []topology.ComputeInstances) (*topology.Vertex, error) {
+	contentType := "Content-Type: application/json"
+	accept := "accept: application/json"
+
+	creds := fmt.Sprintf("{\"username\":\"%s\" , \"password\":\"%s\"}", cred.Uname, cred.Pwd)
+	args := []string{p.NetqLoginUrl, "-H", accept, "-H", contentType, "-d", creds}
+	stdout, err := exec.Exec(ctx, "curl", args, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	//get access code from stdout and call API URL
+	var authOutput AuthOutput
+	outputBytes := stdout.Bytes()
+	if len(outputBytes) == 0 {
+		return nil, fmt.Errorf("failed to login to Netq server\n")
+	}
+
+	if err := json.Unmarshal(outputBytes, &authOutput); err != nil {
+		return nil, fmt.Errorf("failed to parse access token: %v\n", err)
+	}
+
+	addArgs := "{\"filters\": [], \"subgroupNestingDepth\":2}"
+	args = []string{p.NetqApiUrl, "-H", "authorization: Bearer " + authOutput.AccessToken, "-H", contentType, "-d", addArgs}
+	stdout, err = exec.Exec(ctx, "curl", args, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var netqResponse []NetqResponse
+	err = json.Unmarshal(stdout.Bytes(), &netqResponse)
+	if err != nil {
+		return nil, fmt.Errorf("netq output read failed: %v", err)
+	}
+
+	nodes := getNodeList(cis)
+	return parseNetq(netqResponse, nodes)
+}
+
+func getReqNodeMap(nodes []string) map[string]bool {
+	reqNodeMap := make(map[string]bool)
+	for _, nodeName := range nodes {
+		reqNodeMap[nodeName] = true
+	}
+	return reqNodeMap
+}
+
+func sortVertices(root *topology.Vertex) []string {
+	// sort the IDs
+	keys := make([]string, 0, len(root.Vertices))
+	for key := range root.Vertices {
+		keys = append(keys, key)
+	}
+	sort.Strings(keys)
+	return keys
+}
+
+func invalidateExtraNodes(curVertex *topology.Vertex, reqNodeMap map[string]bool, nameMap map[string]string) bool {
+	linkExists := false
+
+	if len(curVertex.Vertices) == 0 {
+		_, linkExists = reqNodeMap[nameMap[curVertex.ID]]
+	} else {
+		keys := sortVertices(curVertex)
+		for _, key := range keys {
+			w := curVertex.Vertices[key]
+			childLink := invalidateExtraNodes(w, reqNodeMap, nameMap)
+			if !childLink {
+				delete(curVertex.Vertices, key)
+			}
+			linkExists = linkExists || childLink
+		}
+	}
+	return linkExists
+}
+
+// parseNetq parses Netq topology output
+func parseNetq(netqResponse []NetqResponse, inputNodes []string) (*topology.Vertex, error) {
+	nodeMap := make(map[string]*topology.Vertex)    // nodeId : Vertex
+	tierMap := make(map[string]int)                 // nodeId : tier
+	inverseTierMap := make(map[int]map[string]bool) // tier : nodeId
+	nameMap := make(map[string]string)              // nodeId : nodeName
+	for _, nodelist := range netqResponse[0].Nodes {
+		for _, cnode := range nodelist.Cnode {
+			nodeMap[cnode.Id] = &topology.Vertex{
+				ID:       cnode.Id,
+				Name:     cnode.Name,
+				Vertices: make(map[string]*topology.Vertex),
+			}
+			tierMap[cnode.Id] = cnode.Tier
+			nameMap[cnode.Id] = cnode.Name
+		}
+	}
+
+	highestTier := -1
+	for _, link := range netqResponse[0].Links {
+		node_id := strings.Split(link.Id, "-*-")
+
+		// ignore mgmt connections on eth0
+		if len(link.CompoundedLinks) == 1 {
+			nodes := strings.Split(link.CompoundedLinks[0].Id, "-*-")
+			src := strings.Split(nodes[0], ":")
+			target := strings.Split(nodes[1], ":")
+			if src[1] == "eth0" || target[1] == "eth0" {
+				continue
+			}
+		}
+		if tierMap[node_id[0]] > tierMap[node_id[1]] {
+			treenode := nodeMap[node_id[0]]
+			treenode.Vertices[node_id[1]] = nodeMap[node_id[1]]
+			if highestTier < tierMap[node_id[0]] {
+				highestTier = tierMap[node_id[0]]
+			}
+			if _, exists := inverseTierMap[tierMap[node_id[0]]]; !exists {
+				inverseTierMap[tierMap[node_id[0]]] = make(map[string]bool)
+			}
+			inverseTierMap[tierMap[node_id[0]]][node_id[0]] = true
+		} else {
+			treenode := nodeMap[node_id[1]]
+			treenode.Vertices[node_id[0]] = nodeMap[node_id[0]]
+			if highestTier < tierMap[node_id[1]] {
+				highestTier = tierMap[node_id[1]]
+			}
+			if _, exists := inverseTierMap[tierMap[node_id[1]]]; !exists {
+				inverseTierMap[tierMap[node_id[1]]] = make(map[string]bool)
+			}
+			inverseTierMap[tierMap[node_id[1]]][node_id[1]] = true
+		}
+	}
+
+	treeRoot := &topology.Vertex{
+		Vertices: make(map[string]*topology.Vertex),
+		Metadata: make(map[string]string),
+	}
+
+	for nodeId := range inverseTierMap[highestTier] {
+		treeRoot.Vertices[nodeId] = nodeMap[nodeId]
+	}
+
+	if len(inputNodes) > 0 {
+		reqNodeMap := getReqNodeMap(inputNodes)
+		invalidateExtraNodes(treeRoot, reqNodeMap, nameMap)
+	}
+
+	var graphVertices []*topology.Vertex
+	for _, node := range treeRoot.Vertices {
+		graphVertices = append(graphVertices, node)
+	}
+
+	// Ethernet Spectrum-X may have CLOS network and may require merging of switches to a tree format
+	merger := topology.NewMerger(graphVertices)
+	merger.Merge()
+	top := merger.TopTier()
+
+	treeRoot = &topology.Vertex{
+		Vertices: make(map[string]*topology.Vertex),
+		Metadata: make(map[string]string),
+	}
+
+	for _, node := range top {
+		treeRoot.Vertices[node.ID] = node
+	}
+
+	root := &topology.Vertex{
+		Vertices: make(map[string]*topology.Vertex),
+	}
+	root.Vertices[topology.TopologyTree] = treeRoot
+
+	return root, nil
+}
--- pkg/providers/baremetal/provider.go
+++ pkg/providers/baremetal/provider.go
@@ -3,14 +3,29 @@ package baremetal
 import (
 	"context"
 	"errors"
+	"fmt"
 
+	"github.com/NVIDIA/topograph/internal/config"
 	"github.com/NVIDIA/topograph/pkg/providers"
 	"github.com/NVIDIA/topograph/pkg/topology"
 )
 
 const NAME = "baremetal"
 
-type Provider struct{}
+type ProviderParams struct {
+	NetqLoginUrl string `mapstructure:"netqLoginUrl"`
+	NetqApiUrl   string `mapstructure:"netqApiUrl"`
+}
+
+type Provider struct {
+	pp   ProviderParams
+	cred Credentials
+}
+
+type Credentials struct {
+	Uname string
+	Pwd   string
+}
 
 var ErrMultiRegionNotSupported = errors.New("on-prem does not support multi-region topology requests")
 
@@ -19,11 +34,42 @@ func NamedLoader() (string, providers.Loader) {
 }
 
 func Loader(ctx context.Context, config providers.Config) (providers.Provider, error) {
-	return New()
+	p, err := GetParams(config.Params)
+	if err != nil {
+		return nil, err
+	}
+	cred, err := GetCred(config.Creds)
+	if err != nil {
+		return nil, err
+	}
+	return New(p, cred)
 }
 
-func New() (*Provider, error) {
-	return &Provider{}, nil
+func New(params *ProviderParams, cred *Credentials) (*Provider, error) {
+	return &Provider{
+		pp:   *params,
+		cred: *cred,
+	}, nil
+}
+
+func GetCred(cred map[string]string) (*Credentials, error) {
+	if _, ok := cred["uname"]; !ok {
+		return nil, fmt.Errorf("error username not provided")
+	}
+
+	if _, ok := cred["pwd"]; !ok {
+		return nil, fmt.Errorf("error username not provided")
+	}
+
+	return &Credentials{Uname: cred["uname"], Pwd: cred["pwd"]}, nil
+}
+
+func GetParams(params map[string]any) (*ProviderParams, error) {
+	var p ProviderParams
+	if err := config.Decode(params, &p); err != nil {
+		return nil, fmt.Errorf("error decoding params: %w", err)
+	}
+	return &p, nil
 }
 
 func (p *Provider) GenerateTopologyConfig(ctx context.Context, _ *int, instances []topology.ComputeInstances) (*topology.Vertex, error) {
@@ -31,11 +77,12 @@ func (p *Provider) GenerateTopologyConfig(ctx context.Context, _ *int, instances
 		return nil, ErrMultiRegionNotSupported
 	}
 
+	if len(p.pp.NetqLoginUrl) > 0 {
+		return generateTopologyConfigForEth(ctx, p.cred, p.pp, instances)
+	}
 	//call mnnvl code from here
 	return generateTopologyConfig(ctx, instances)
 }
-
-// Engine support
 
 // Instances2NodeMap implements slurm.instanceMapper
 func (p *Provider) Instances2NodeMap(ctx context.Context, nodes []string) (map[string]string, error) {
